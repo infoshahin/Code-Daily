260b7502859a42eba6b62dc7ea1e31f706807697
irfankazi <irfan.rv15@gmail.com>
2014-09-23 11:07:46 +0600

    Xml Parsing
    XML Parsing in .NET for 22nd and 23rd September & HTML code for XML
    Parsing in 23rd September

---
+++ 22-09-2014/XmlParsing/XmlParsing/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace XmlParsing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlDocument xmlDocument = new XmlDocument();
+            //XmlDocument xmlDocument = new XmlDocument();
+            xmlDocument.Load(@"E:\KAZI\Office Docs\String Parsing.xml");
+
+            XmlNodeList nodesList = xmlDocument.DocumentElement.SelectNodes("/item");
+
+            List<Item> items = new List<Item>();
+
+            foreach (XmlNode node in nodesList)
+            {
+                Item item = new Item();
+                item.key = node.SelectSingleNode("key").InnerText;
+                item.value = node.SelectSingleNode("value").InnerText;
+
+                items.Add(item);
+            }
+
+            Console.WriteLine(items);
+            Console.ReadKey();
+        }
+    }
+}
