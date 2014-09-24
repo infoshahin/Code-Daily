52a2a0a7b95a161d6afc362a09d514d9c93d8b1a
irfankazi <irfan.rv15@gmail.com>
2014-09-24 11:37:06 +0600

    Simple XML Parsing Done

---
+++ 24-09-2014/SimpleXmlParsing/SimpleXmlParsing/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace SimpleXmlParsing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           XmlReader xmlReader = XmlReader.Create(@"E:\KAZI\Works\24-09-2014\SimpleXmlParsing\SimpleXmlParsing\XmlData.xml");
+            while (xmlReader.Read())
+            {
+                if (xmlReader.IsStartElement())
+                {
+                    switch (xmlReader.Name.ToString())
+                    {
+                        case "Name":
+                            Console.WriteLine("Element: " + xmlReader.ReadString());
+                            break;
+
+                        case "Location":
+                            Console.WriteLine("Location: " + xmlReader.ReadString());
+                            break;
+                    }
+                }
+
+                Console.WriteLine("");
+            }
+            Console.ReadKey();
+        }
+    }
+}
