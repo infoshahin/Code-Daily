5688f00668dfa273aaddff839e568358b06c0772
irfankazi <irfan.rv15@gmail.com>
2014-09-30 13:33:15 +0600

    Http Request Done

---
+++ 30-09-2014/HttpRequest/WindowsFormsApplication1/DBConnection.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    class DBConnection
+    {
+       
+
+        public System.Data.SqlClient.SqlConnection conn;
+
+        public DBConnection()
+
+        {
+
+            //constructor
+
+        }
+
+        ~DBConnection()
+
+        {
+
+            //destructor
+
+            conn = null;
+
+        }
+
+        public void Disconnect()
+
+        {
+
+            conn.Close();
+
+        }
+
+        public string ConnectToDatabase()
+
+        {
+
+            try
+
+            {
+                conn = new System.Data.SqlClient.SqlConnection();
+                conn.ConnectionString = @"Data Source=.\SQLEXPRESS;AttachDbFilename=E:\KAZI\Works\29-09-2014\2013 VS\WindowsFormsApplication1\WindowsFormsApplication1\Database1.mdf;Integrated Security=True;Connect Timeout=30;User Instance=True";
+                conn.Open();
+                return "Connected";
+
+            }
+
+            catch(SqlException e)
+
+            {
+
+                conn = null;
+
+                return e.Message;
+
+            }
+
+        }
+        public DataTable getAllDrugs()
+        {
+
+            try
+            {
+
+                string strCommand = "SELECT * FROM tblSub";
+
+                DataTable dt = new DataTable();
+
+                SqlDataAdapter da = new SqlDataAdapter(strCommand, conn);
+
+                da.Fill(dt);
+
+                return dt;
+
+            }
+
+            catch (SqlException e)
+            {
+
+                MessageBox.Show(e.Source + "\n" + e.Message + "\n" + e.StackTrace);
+
+                return null;
+
+            }
+
+        }
+    }
+}
+
---
+++ 30-09-2014/HttpRequest/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using WindowsFormsApplication1;
+using System.IO;
+using System.Xml;
+using System.Xml.Linq;
+using System.Data.SqlClient;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        Timer _RefreshFormTimer = new Timer();
+        public Form1()
+        {
+            InitializeComponent();
+            _RefreshFormTimer.Interval = 1000 * 30; //3 seconds.
+            _RefreshFormTimer.Tick += new EventHandler(RefreshFormTimer_Tick);
+            _RefreshFormTimer.Enabled = false;
+        }
+        DBConnection dbCon = new DBConnection();
+        string read_msisdn;
+        string read_sms;
+        private void submit_Click(object sender, EventArgs e)
+        {
+
+            string val1 = txtName.Text;
+            string val2 = txtNumber.Text;
+            string ins_msisdn = txtNumber.Text;
+            string ins_sms = txtName.Text;
+
+
+            
+            string insCommand = "INSERT INTO tblSub (sms,msisdn) VALUES (@msisdn,@sms)";
+            SqlCommand cmd = new SqlCommand(insCommand, dbCon.conn);
+                cmd.Parameters.Add("@msisdn", SqlDbType.NChar).Value = ins_msisdn;
+                cmd.Parameters.Add("@sms", SqlDbType.NChar).Value = ins_sms;
+                cmd.ExecuteNonQuery();
+                dbStatusLabel1.Text = "Data Added";
+                txtName.Clear();
+                txtNumber.Clear();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dbStatusLabel1.Text = dbCon.ConnectToDatabase(); ;
+            if (dbStatusLabel1.Text == "Connected") {
+            }
+            
+            
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            dbCon.Disconnect();
+        }
+        private void RefreshFormTimer_Tick(object sender, EventArgs e)
+        {
+            listView1.Clear();
+            listView1.AutoArrange = true;
+
+            listView1.Columns.Add("STATUS");
+            listView1.Columns.Add("MSISDN");
+            listView1.Columns.Add("SMS");
+            listView1.Columns.Add("ERROR");
+            for (var i = 0; i < listView1.Columns.Count; i++)
+            {
+                listView1.Columns[i].Width = 150;
+                listView1.Columns[i].TextAlign = HorizontalAlignment.Center;
+            }
+            //Retrive Data from Database
+            SqlCommand com = new SqlCommand("SELECT msisdn, sms FROM tblSub", dbCon.conn);
+            SqlDataReader reader = com.ExecuteReader();
+            while (reader.Read())
+            {
+                read_msisdn = reader.GetString(0);  // msisdn string
+                read_sms = reader.GetString(1); // sms string
+                //
+                // Write the values read from the database to the screen.
+                //
+                //create the constructor with post type and few data
+                MyWebRequest myRequest = new MyWebRequest("http://192.168.61.144/subscription/test/test.php", "POST", "msisdn=" + read_msisdn + "&sms=" + read_sms);
+                //show the response string on the console screen.
+
+                label1.Text = myRequest.GetResponse();
+                XDocument doc = XDocument.Parse(label1.Text);
+
+                var resDatas = doc.Descendants("REPLY").Select(datas => new
+                {
+                    Status = datas.Element("STATUS").Value,
+                    Msisdn = datas.Element("MSISDN").Value,
+                    SMS = datas.Element("SMS").Value,
+                    Error = datas.Element("ERROR").Value
+                }).ToList();
+                foreach (var datas in resDatas)
+                {
+                    ListViewItem lvi = new ListViewItem(datas.Status);
+                    lvi.SubItems.Add(datas.Msisdn);
+                    lvi.SubItems.Add(datas.SMS);
+                    lvi.SubItems.Add(datas.Error);
+                    listView1.Items.Add(lvi);
+                }
+            }
+            reader.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Process started");
+            _RefreshFormTimer.Enabled = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            _RefreshFormTimer.Enabled = false;
+            listView1.Clear();
+            MessageBox.Show("Process Stopped");
+            listView1.Clear();
+            listView1.AutoArrange = true;
+
+            listView1.Columns.Add("STATUS");
+            listView1.Columns.Add("MSISDN");
+            listView1.Columns.Add("SMS");
+            listView1.Columns.Add("ERROR");
+            for (var i = 0; i < listView1.Columns.Count; i++)
+            {
+                listView1.Columns[i].Width = 150;
+                listView1.Columns[i].TextAlign = HorizontalAlignment.Center;
+            }
+            //Retrive Data from Database
+            SqlCommand com = new SqlCommand("SELECT msisdn, sms FROM tblSub", dbCon.conn);
+            SqlDataReader reader = com.ExecuteReader();
+            while (reader.Read())
+            {
+                read_msisdn = reader.GetString(0);  // msisdn string
+                read_sms = reader.GetString(1); // sms string
+                //
+                // Write the values read from the database to the screen.
+                //
+                //create the constructor with post type and few data
+                MyWebRequest myRequest = new MyWebRequest("http://192.168.61.144/subscription/test/test.php", "POST", "msisdn=" + read_msisdn + "&sms=" + read_sms);
+                //show the response string on the console screen.
+
+                label1.Text = myRequest.GetResponse();
+                XDocument doc = XDocument.Parse(label1.Text);
+
+                var resDatas = doc.Descendants("REPLY").Select(datas => new
+                {
+                    Status = datas.Element("STATUS").Value,
+                    Msisdn = datas.Element("MSISDN").Value,
+                    SMS = datas.Element("SMS").Value,
+                    Error = datas.Element("ERROR").Value
+                }).ToList();
+                foreach (var datas in resDatas)
+                {
+                    ListViewItem lvi = new ListViewItem(datas.Status);
+                    lvi.SubItems.Add(datas.Msisdn);
+                    lvi.SubItems.Add(datas.SMS);
+                    lvi.SubItems.Add(datas.Error);
+                    listView1.Items.Add(lvi);
+                }
+            }
+            reader.Close();
+        }
+    }
+}
