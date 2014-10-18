17703401400b13c44d06b939778f984c0a45e7ee
irfankazi <irfan.rv15@gmail.com>
2014-10-18 17:02:48 +0600

    Web Service Using PHP
    A simple web service is created using PHP and the web service is used in
    a program

---
+++ 16-10-2014/Web Service PHP/WebServiceSOAP (2)/WebServiceSOAP/service.php
@@ -0,0 +1,27 @@
+<?php
+	$client = new SoapClient("https://localhost/WebServiceSOAP/server.php?wsdl");
+	
+	$result = $client->login('john', 'dgoe');
+	
+	$response_arr = objectToArray($result);
+	
+	echo "Full Name = " . str_replace(";", "", $response_arr["fullname"])."<br/>";
+	echo "Message = " . str_replace(";", "", $response_arr["level"])."<br/>";
+	
+	function objectToArray($d) 
+	{
+		if (is_object($d)) 
+		{
+			$d = get_object_vars($d);
+		}
+ 
+		if (is_array($d)) 
+		{
+			return array_map(__FUNCTION__, $d);
+		}
+		else 
+		{
+			return $d;
+		}
+	}
+?>
