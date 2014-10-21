4e36fe3e3bb65c072e160352802947bc792ae1fe
irfankazi <irfan.rv15@gmail.com>
2014-10-21 17:26:17 +0600

    Robi App Get, Soap Complete

---
+++ 21-10-2014(Robi App Get, Soap Complete)/SendSmsApp/SendSmsApp/Default.aspx.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Security.Cryptography;
+using System.Web;
+using System.Web.Services.Description;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using SendSmsApp.bd.com.robi.bmpws;
+
+namespace SendSmsApp
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        public const string username = "TestSMSRobi";
+        public const string password = "TestSMSRobi";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void sendMessageButtonGet_Click(object sender, EventArgs e)
+        {
+            string getUrl = "https://bmpws.robi.com.bd/ApacheGearWS/SendTextMessage";
+
+            string getFrom = fromTextBoxGet.Text;
+            string getTo = toTextBoxGet.Text;
+            string getMessage = textMsgTextBoxGet.Text;
+            string getParameters = "Username=" + username + "&" + "Password=" + password + "&" + "From=" + getFrom + "&" + "To=" + getTo + "&" + "Message=" + getMessage;
+            string getResult = SmsThroughHttpGet(getUrl, getParameters);
+            getLabel.Text = getResult;
+        }
+
+        private string SmsThroughHttpGet(string getUrl, string getParameters)
+        {
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(getUrl + "?" + getParameters);
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                Stream stream = response.GetResponseStream();
+                StreamReader reader = new StreamReader(stream);
+
+                string data = reader.ReadToEnd().Replace("?", " ");
+
+                reader.Close();
+                stream.Close();
+
+                return data;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        protected void sendMessageButtonSoap_Click(object sender, EventArgs e)
+        {
+            string from = fromTextBoxSoap.Text;
+            string to = toTextBoxSoap.Text;
+            string message = textMsgTextBoxSoap.Text;
+            string sendTextMessageToOneRecipientId = SmsThroughSoap(username, password, from, to, message);
+            soapLabel.Text = sendTextMessageToOneRecipientId;
+        }
+
+        private string SmsThroughSoap(string username, string password, string from, string to, string message)
+        {
+            bd.com.robi.bmpws.CMPWebService webService = new CMPWebService();
+            var sendTextMessageToOneRecipient = webService.SendTextMessage(username, password, from, to, message);
+            return sendTextMessageToOneRecipient.MessageId.ToString();
+        }
+    }
+}
