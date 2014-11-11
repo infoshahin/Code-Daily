f4afc6e053c922d2812ed2f10a0a84ed08320c57
irfankazi <irfan.rv15@gmail.com>
2014-11-11 12:46:45 +0600

    JSON taks upto data shown

---
+++ 11-11-2014(JSON)/JSON Upto Data Shown/JsonCrownCement/JsonCrownCement/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace JsonCrownCement
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.showJsonData = new System.Windows.Forms.Label();
+            this.showDbStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // showJsonData
+            // 
+            this.showJsonData.AutoSize = true;
+            this.showJsonData.Location = new System.Drawing.Point(73, 71);
+            this.showJsonData.Name = "showJsonData";
+            this.showJsonData.Size = new System.Drawing.Size(91, 13);
+            this.showJsonData.TabIndex = 0;
+            this.showJsonData.Text = "Show JSON Data";
+            // 
+            // showDbStatus
+            // 
+            this.showDbStatus.AutoSize = true;
+            this.showDbStatus.Location = new System.Drawing.Point(73, 247);
+            this.showDbStatus.Name = "showDbStatus";
+            this.showDbStatus.Size = new System.Drawing.Size(55, 13);
+            this.showDbStatus.TabIndex = 1;
+            this.showDbStatus.Text = "DB Status";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(814, 303);
+            this.Controls.Add(this.showDbStatus);
+            this.Controls.Add(this.showJsonData);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label showJsonData;
+        private System.Windows.Forms.Label showDbStatus;
+    }
+}
+
---
+++ 11-11-2014(JSON)/JSON Upto Data Shown/JsonCrownCement/JsonCrownCement/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace JsonCrownCement
+{
+    public partial class Form1 : Form
+    {
+        Timer refreshFormTimer = new Timer();
+        DBConnection newDBConnection = new DBConnection();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ConvertDataTabletoStringFromDealerTable();
+        }
+
+        public string ConvertDataTabletoStringFromDealerTable()
+        {
+            try
+            {
+                DataTable dt = new DataTable();
+
+                showDbStatus.Text = newDBConnection.ConnectToDatabase();
+                string queryToSelect = "SELECT * FROM dealer";
+                SqlCommand cmd = new SqlCommand(queryToSelect, newDBConnection.conn);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                da.Fill(dt);
+                string[] jsonArray = new string[dt.Columns.Count];
+                string headString = string.Empty;
+
+                for (int i = 0; i < dt.Columns.Count; i++)
+                {
+                    jsonArray[i] = dt.Columns[i].Caption; // Array for all columns
+                    headString += "'" + jsonArray[i] + "' : '" + jsonArray[i] + i.ToString() + "%" + "',";
+                }
+
+                headString = headString.Substring(0, headString.Length - 1);
+
+                StringBuilder sb = new StringBuilder();
+                sb.Append("[");
+
+                if (dt.Rows.Count > 0)
+                {
+                    for (int i = 0; i < dt.Rows.Count; i++)
+                    {
+                        string tempString = headString;
+                        sb.Append("{");
+
+                        // To get each value from the datatable
+                        for (int j = 0; j < dt.Columns.Count; j++)
+                        {
+                            tempString = tempString.Replace(dt.Columns[j] + j.ToString() + "%", dt.Rows[i][j].ToString());
+                        }
+
+                        sb.Append(tempString + "},");
+                    }
+                }
+                else
+                {
+                    string tempString = headString;
+                    sb.Append("{");
+                    for (int j = 0; j < dt.Columns.Count; j++)
+                    {
+                        tempString = tempString.Replace(dt.Columns[j] + j.ToString() + "%", "-");
+                    }
+
+                    sb.Append(tempString + "},");
+                }
+
+                sb = new StringBuilder(sb.ToString().Substring(0, sb.ToString().Length - 1));
+                sb.Append("]");
+                showJsonData.Text = sb.ToString();
+                StreamWriter outfile = new StreamWriter(@"JsonForDealer.txt");
+                outfile.Write(sb);
+                return showJsonData.Text;
+            }
+            catch (Exception exception)
+            {
+                showJsonData.Text = exception.Message;
+                return showJsonData.Text;
+            }
+        }
+    }
+}
