1c33f88d131a7a454a53b2c576e2d50c83500b46
irfankazi <irfan.rv15@gmail.com>
2014-12-23 16:49:07 +0600

    Check API by .NET

---
+++ 23-12-2014(API Check & Test)/Check API/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using WindowsFormsApplication1.WebReference;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SoapProvisioning spv=new SoapProvisioning();
+            string result = spv.recharge("test20111027a", "012345678", "1234", "");
+            label1.Text = result;
+        }
+    }
+}
